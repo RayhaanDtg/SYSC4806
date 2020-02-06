e558f5ab02c6489898de059099d0f78de22660b8
RayhaanDtg <48567489+RayhaanDtg@users.noreply.github.com>
2020-02-05 22:30:27 -0500

    Update AddressBook.java

--- src/main/java/App/AddressBook.java
+++ src/main/java/App/AddressBook.java
@@ -6,17 +6,23 @@ import javax.persistence.OneToMany;
 import java.io.Serializable;
 import java.util.* ;
 
-@Entity
+// must annotate the class with persistence annotations to achieve ORM into database
+// this uses the java persistence library to map the ORM and spring configures these
+// annotations to store into its own database.
+@Entity // indicates that this class is a table in database
 public class AddressBook implements Serializable {
 
-
+// indicates that this should be a field of one to many relationship in the table
     @OneToMany
     private List<BuddyInfo> addressBook;
 
+    // shows that this field will be used as the ID field for each row in the table
     @Id
     private Long id = null;
 
-
+// the constructors, enable creation of an object to be pu in the database
+// each object maps into a row
+// getters and setters are implemented to manipulate the field of this class
 
     public AddressBook() {
 
